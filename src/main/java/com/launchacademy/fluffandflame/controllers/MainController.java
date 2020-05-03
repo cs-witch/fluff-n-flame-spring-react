e0eb41975566e1a1dcdba1e4e4237b96ee894ef3
Andisa Montez <andisa.andisa@gmail.com>
2020-05-03 17:25:33 -0400

    index page mapping

---
+++ src/main/java/com/launchacademy/fluffandflame/controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.launchacademy.fluffandflame.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+  @GetMapping(value = {"/", "/creatures"})
+  public String index() {
+    return "index";
+  }
+
+  @GetMapping(value = "/**/{path:[^\\.]*}")
+  public String forward() {
+    return "forward:/";
+  }
+}
